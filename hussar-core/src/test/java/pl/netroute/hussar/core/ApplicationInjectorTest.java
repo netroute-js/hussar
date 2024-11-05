c1789373ab57966004c18faa47410841a0bab746
sjachowicz <sjachowicz@netroute.pl>
2024-11-05 19:57:00 +0100

    fix: inheritance Service and Application injection.

--- hussar-core/src/test/java/pl/netroute/hussar/core/ApplicationInjectorTest.java
+++ hussar-core/src/test/java/pl/netroute/hussar/core/ApplicationInjectorTest.java
@@ -2,8 +2,8 @@ package pl.netroute.hussar.core;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import pl.netroute.hussar.core.api.application.HussarApplication;
 import pl.netroute.hussar.core.api.application.Application;
+import pl.netroute.hussar.core.api.application.HussarApplication;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -32,11 +32,34 @@ public class ApplicationInjectorTest {
         assertApplicationInjectionSkipped(testInstance);
     }
 
+    @Test
+    public void shouldInjectApplicationWhenInheritancePresent() {
+        // given
+        var testInstance = new SubTestClassWithApplication();
+
+        // when
+        applicationInjector.inject(testInstance);
+
+        // then
+        assertApplicationInjected(testInstance, application);
+        assertApplicationInjectionSkipped(testInstance);
+    }
+
     private void assertApplicationInjected(TestClassWithApplication testInstance, Application application) {
         assertThat(testInstance.application).isEqualTo(application);
     }
 
+    private void assertApplicationInjected(SubTestClassWithApplication testInstance, Application application) {
+        assertThat(testInstance.baseApplication).isEqualTo(application);
+        assertThat(testInstance.application).isEqualTo(application);
+    }
+
     private void assertApplicationInjectionSkipped(TestClassWithApplication testInstance) {
+        assertThat(testInstance.nonInjectableApplication).isNull();
+    }
+
+    private void assertApplicationInjectionSkipped(SubTestClassWithApplication testInstance) {
+        assertThat(testInstance.baseNonInjectableApplication).isNull();
         assertThat(testInstance.nonInjectableApplication).isNull();
     }
 
@@ -47,6 +70,22 @@ public class ApplicationInjectorTest {
 
         private Application nonInjectableApplication;
 
+    }
+
+    private static class BaseTestClassWithApplication {
+
+        @HussarApplication
+        Application baseApplication;
+
+        Application baseNonInjectableApplication;
+    }
+
+    private static class SubTestClassWithApplication extends BaseTestClassWithApplication {
+
+        @HussarApplication
+        Application application;
+
+        Application nonInjectableApplication;
     }
 
 }
