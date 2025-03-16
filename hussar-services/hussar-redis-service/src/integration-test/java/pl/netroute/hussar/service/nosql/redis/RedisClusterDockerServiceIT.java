958abd104b954ed6eaf99ec4c9adc1aadba46273
sjachowicz <sjachowicz@netroute.pl>
2025-03-16 17:53:19 +0100

    feature: RedisCluster docker service ITs.

--- hussar-services/hussar-redis-service/src/integration-test/java/pl/netroute/hussar/service/nosql/redis/RedisClusterDockerServiceIT.java
+++ hussar-services/hussar-redis-service/src/integration-test/java/pl/netroute/hussar/service/nosql/redis/RedisClusterDockerServiceIT.java
@@ -41,4 +41,91 @@ public class RedisClusterDockerServiceIT {
         redisClusterAssertion.assertNoEntriesRegistered();
     }
 
+    @Test
+    public void shouldStartSecuredRedisClusterService() {
+        // given
+        redisClusterService = RedisClusterDockerServiceConfigurer
+                .newInstance()
+                .enablePassword(true)
+                .done()
+                .configure(ServiceConfigureContext.defaultContext());
+
+        // when
+        redisClusterService.start(ServiceStartupContext.defaultContext());
+
+        // then
+        var redisClusterAssertion = new RedisClusterAssertionHelper(redisClusterService);
+        redisClusterAssertion.assertMultipleEndpoints();
+        redisClusterAssertion.asserRedisClusterAccessible();
+        redisClusterAssertion.assertNoEntriesRegistered();
+    }
+
+    @Test
+    public void shouldStartRedisClusterServiceWithFullConfiguration() {
+        // given
+        var name = "redis-cluster-instance";
+        var dockerVersion = "6.2.0";
+
+        var endpointProperty = "redis.cluster.url";
+        var endpointEnvVariable = "REDIS_CLUSTER_URL";
+
+        var usernameProperty = "redis.cluster.username";
+        var usernameEnvVariable = "REDIS_CLUSTER_USERNAME";
+
+        var passwordProperty = "redis.cluster.password";
+        var passwordEnvVariable = "REDIS_CLUSTER_PASSWORD";
+
+        redisClusterService = RedisClusterDockerServiceConfigurer
+                .newInstance()
+                .name(name)
+                .dockerImageVersion(dockerVersion)
+                .enablePassword(true)
+                .registerEndpointUnderProperty(endpointProperty)
+                .registerEndpointUnderEnvironmentVariable(endpointEnvVariable)
+                .registerUsernameUnderProperty(usernameProperty)
+                .registerUsernameUnderEnvironmentVariable(usernameEnvVariable)
+                .registerPasswordUnderProperty(passwordProperty)
+                .registerPasswordUnderEnvironmentVariable(passwordEnvVariable)
+                .done()
+                .configure(ServiceConfigureContext.defaultContext());
+
+        // when
+        redisClusterService.start(ServiceStartupContext.defaultContext());
+
+        // then
+        var redisClusterAssertion = new RedisClusterAssertionHelper(redisClusterService);
+        redisClusterAssertion.assertMultipleEndpoints();
+        redisClusterAssertion.asserRedisClusterAccessible();
+        redisClusterAssertion.assertRegisteredEndpointsUnderProperty(endpointProperty);
+        redisClusterAssertion.assertRegisteredEndpointsUnderEnvironmentVariable(endpointEnvVariable);
+        redisClusterAssertion.assertRegisteredUsernameUnderProperty(usernameProperty);
+        redisClusterAssertion.assertRegisteredUsernameUnderEnvironmentVariable(usernameEnvVariable);
+        redisClusterAssertion.assertRegisteredPasswordUnderProperty(passwordProperty);
+        redisClusterAssertion.assertRegisteredPasswordUnderEnvironmentVariable(passwordEnvVariable);
+    }
+
+    @Test
+    public void shouldShutdownRedisClusterService() {
+        var name = "redis-cluster-instance";
+        var dockerVersion = "6.2.0";
+
+        redisClusterService = RedisClusterDockerServiceConfigurer
+                .newInstance()
+                .name(name)
+                .dockerImageVersion(dockerVersion)
+                .done()
+                .configure(ServiceConfigureContext.defaultContext());
+
+        // when
+        redisClusterService.start(ServiceStartupContext.defaultContext());
+
+        var endpoints = redisClusterService.getEndpoints();
+
+        redisClusterService.shutdown();
+
+        // then
+        var redisClusterAssertion = new RedisClusterAssertionHelper(redisClusterService);
+        redisClusterAssertion.assertRedisClusterNotAccessible(endpoints);
+    }
+
 }
--- hussar-services/hussar-redis-service/src/integration-test/java/pl/netroute/hussar/service/nosql/redis/assertion/RedisClusterAssertionHelper.java
+++ hussar-services/hussar-redis-service/src/integration-test/java/pl/netroute/hussar/service/nosql/redis/assertion/RedisClusterAssertionHelper.java
@@ -3,12 +3,16 @@ package pl.netroute.hussar.service.nosql.redis.assertion;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import pl.netroute.hussar.core.api.Endpoint;
+import pl.netroute.hussar.core.configuration.api.ConfigurationEntry;
+import pl.netroute.hussar.core.configuration.api.EnvVariableConfigurationEntry;
+import pl.netroute.hussar.core.configuration.api.PropertyConfigurationEntry;
 import pl.netroute.hussar.service.nosql.redis.api.RedisClusterDockerService;
 import pl.netroute.hussar.service.nosql.redis.api.RedisCredentials;
 import redis.clients.jedis.DefaultJedisClientConfig;
 import redis.clients.jedis.DefaultRedisCredentials;
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.exceptions.JedisClusterOperationException;
 
 import java.time.Duration;
 import java.util.List;
@@ -24,6 +28,7 @@ public class RedisClusterAssertionHelper {
     private static final Duration TIMEOUT = Duration.ofSeconds(20L);
 
     private static final String PING_RESULT = "PONG";
+    private static final String ENDPOINTS_JOIN_DELIMITER = ",";
 
     @NonNull
     private final RedisClusterDockerService redisCluster;
@@ -42,12 +47,74 @@ public class RedisClusterAssertionHelper {
         }
     }
 
+    public void assertRedisClusterNotAccessible(@NonNull List<Endpoint> endpoints) {
+        try(var client = createClient(endpoints)) {
+            throw new AssertionError("Expected JedisClusterOperationException");
+        } catch (JedisClusterOperationException ex) {
+        } catch (Exception ex) {
+            throw new AssertionError("Expected JedisClusterOperationException");
+        }
+    }
+
+    public void assertRegisteredEndpointsUnderProperty(@NonNull String registeredProperty) {
+        var endpoints = redisCluster.getEndpoints();
+        var squashedEndpoints = squashEndpoints(endpoints);
+
+        assertRegisteredEntryInConfigRegistry(registeredProperty, squashedEndpoints, PropertyConfigurationEntry.class);
+    }
+
+    public void assertRegisteredEndpointsUnderEnvironmentVariable(@NonNull String registeredEnvVariable) {
+        var endpoints = redisCluster.getEndpoints();
+        var squashedEndpoints = squashEndpoints(endpoints);
+
+        assertRegisteredEntryInConfigRegistry(registeredEnvVariable, squashedEndpoints, EnvVariableConfigurationEntry.class);
+    }
+
+    public void assertRegisteredUsernameUnderProperty(@NonNull String registeredProperty) {
+        var credentials = redisCluster.getCredentials();
+
+        assertRegisteredEntryInConfigRegistry(registeredProperty, credentials.username(), PropertyConfigurationEntry.class);
+    }
+
+    public void assertRegisteredUsernameUnderEnvironmentVariable(@NonNull String registeredEnvVariable) {
+        var credentials = redisCluster.getCredentials();
+
+        assertRegisteredEntryInConfigRegistry(registeredEnvVariable, credentials.username(), EnvVariableConfigurationEntry.class);
+    }
+
+    public void assertRegisteredPasswordUnderProperty(@NonNull String registeredProperty) {
+        var credentials = redisCluster.getCredentials();
+
+        assertRegisteredEntryInConfigRegistry(registeredProperty, credentials.password(), PropertyConfigurationEntry.class);
+    }
+
+    public void assertRegisteredPasswordUnderEnvironmentVariable(@NonNull String registeredEnvVariable) {
+        var credentials = redisCluster.getCredentials();
+
+        assertRegisteredEntryInConfigRegistry(registeredEnvVariable, credentials.password(), EnvVariableConfigurationEntry.class);
+    }
+
     public void assertNoEntriesRegistered() {
         var entriesRegistered = redisCluster
                 .getConfigurationRegistry()
                 .getEntries();
 
         assertThat(entriesRegistered).isEmpty();
+    }
+
+    private void assertRegisteredEntryInConfigRegistry(String entryName, String entryValue, Class<? extends ConfigurationEntry> configType) {
+        var configRegistry = redisCluster.getConfigurationRegistry();
+
+        configRegistry
+                .getEntries()
+                .stream()
+                .filter(configEntry -> configEntry.getClass().equals(configType))
+                .filter(configEntry -> configEntry.name().equals(entryName))
+                .findFirst()
+                .ifPresentOrElse(
+                        configEntry -> assertThat(configEntry.value()).isEqualTo(entryValue),
+                        () -> { throw new AssertionError("Expected registered entry in config registry. Found none"); }
+                );
     }
 
     private JedisCluster createClient(List<Endpoint> endpoints) {
@@ -79,6 +146,13 @@ public class RedisClusterAssertionHelper {
         var passwordChars = credentials.password().toCharArray();
 
         return new DefaultRedisCredentials(username, passwordChars);
+    }
+
+    private String squashEndpoints(List<Endpoint> endpoints) {
+        return endpoints
+                .stream()
+                .map(Endpoint::address)
+                .collect(Collectors.joining(ENDPOINTS_JOIN_DELIMITER));
     }
 
 }
